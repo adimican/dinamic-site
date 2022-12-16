bad978b9112de6915c5b71a59645b3dd023ba949
adimican2 <adimican@gmail.com>
2022-12-16 08:11:20 +0200

    modify path for dinamic site anchor

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-const express = require('express');
+const path = require("path");
+
+const express = require("express");
 
 const app = express();
 
-app.get('/', function(req,res)
-{
-    res.send('<h1>Hello world!</h1>')
+
+app.get("/about", function (req, res) {
+  const pathFile = path.join(__dirname, "views", "about.html");
+  res.sendFile(pathFile);
+});
+
+app.get("/confirm", function (req, res) {
+  const pathFile = path.join(__dirname, "views", "confirm.html");
+  res.sendFile(pathFile);
+});
+
+app.get("/", function (req, res) {
+  const pathFile = path.join(__dirname, "views", "index.html");
+  res.sendFile(pathFile);
+});
+
+app.get("/recommend", function (req, res) {
+    const pathFile = path.join(__dirname, "views", "recommend.html");
+    res.sendFile(pathFile);
+  });
+  
+app.get("/restaurants", function (req, res) {
+  const pathFile = path.join(__dirname, "views", "restaurants.html");
+  res.sendFile(pathFile);
 });
 
 app.listen(3000);
