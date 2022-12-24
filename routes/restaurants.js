9a9aa471767ca5ae33ae598e3bfc527131c6706d
adimican2 <adimican@gmail.com>
2022-12-24 09:54:21 +0200

    creat  a button to sort asc and desc restaurants browse

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,51 +1,71 @@
-const express = require('express');
+const express = require("express");
 
 const resData = require("../util/restaurant-data");
 
 const uuid = require("uuid");
 
-
 const router = express.Router();
 
-
-
 router.get("/confirm", function (req, res) {
-    res.render("confirm");
-  });
-  
-  
-  router.get("/recommend", function (req, res) {
-    res.render("recommend");
-  });
-  
-  router.post("/recommend", function (req, res) {
-    const restaurant = req.body;
-    restaurant.id = uuid.v4();//creat a id prprietate generand un id cu ajutorul uuid metoda v4
-    const restaurants = resData.getStoredResataurants ();
-   restaurants.push(restaurant);
-   resData.storeRestaurants(restaurants);
-    res.redirect("/confirm");
-  });
-  
-  router.get("/restaurants", function (req, res) {
-    const storedRestaurants = resData.getStoredResataurants();
-    res.render("restaurants", {
-      numberOfRestaurants: storedRestaurants.length,
-      restaurants: storedRestaurants,
-    });
-  });
-  
-  router.get('/restaurants/:id', function (req, res) {
-    const restaurantId = req.params.id;
-    const storedRestaurants = resData.getStoredResataurants();
-    
-    for (const restaurant  of storedRestaurants){
-      if (restaurant.id === restaurantId){
-         return res.render("restaurant-detail", { restaurant: restaurant });
-  
-      }
+  res.render("confirm");
+});
+
+router.get("/recommend", function (req, res) {
+  res.render("recommend");
+});
+
+router.post("/recommend", function (req, res) {
+  const restaurant = req.body;
+  restaurant.id = uuid.v4(); //creat a id prprietate generand un id cu ajutorul uuid metoda v4
+  const restaurants = resData.getStoredResataurants();
+  restaurants.push(restaurant);
+  resData.storeRestaurants(restaurants);
+  res.redirect("/confirm");
+});
+
+router.get("/restaurants", function (req, res) {
+  // let order = res.query.order;
+  // let nextOrder = 'desc';
+  let order = req.query.order;
+  let nextOrder = 'desc';
+
+  if (order !== 'asc' && order !== 'desc') {
+    order = 'asc';
+  }
+
+  if (order === 'desc') {
+    nextOrder = 'asc';
+  }
+
+  const storedRestaurants = resData.getStoredResataurants();
+
+storedRestaurants.sort(function (resA, resB) {
+    if (
+      (order === 'asc' && resA.name > resB.name) ||
+      (order === 'desc' && resB.name > resA.name)
+    ) {
+      return 1;
     }
-    res.status(404).render('404');
+    return -1;
+  });
+
+ res.render('restaurants', {
+    numberOfRestaurants: storedRestaurants.length,
+    restaurants: storedRestaurants,
+    nextOrder: nextOrder
+  });
+});
+
+router.get("/restaurants/:id", function (req, res) {
+  const restaurantId = req.params.id;
+  const storedRestaurants = resData.getStoredResataurants();
+
+  for (const restaurant of storedRestaurants) {
+    if (restaurant.id === restaurantId) {
+      return res.render("restaurant-detail", { restaurant: restaurant });
+    }
+  }
+  res.status(404).render("404");
 });
 
 module.exports = router;
